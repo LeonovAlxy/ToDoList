0c2cdb1d220389e2fecb0888cdfc6519a918d730
Aliaksei Leanovich <lleonovalexeyl@gmail.com>
2026-01-11 15:05:20 +0300

    feat:  validation

--- src/InputTasks.jsx
+++ src/InputTasks.jsx
@@ -3,30 +3,45 @@ import { TasksContext } from "./TasksContext";
 const InputTask = () => {
   const { tasks, setTasks } = useContext(TasksContext);
   const [text, setText] = useState("");
+  const [error, setError] = useState("");
+
   const handleChange = (e) => {
     setText(e.target.value);
   };
+
   const handleClick = () => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        id: crypto.randomUUID(),
-        title: text,
-        isDone: false,
-      },
-    ]);
-    setText("");
+    if (text.trim() === "") {
+      setError("Название не может быть пустым или состоять только из пробелов");
+    } else {
+      setTasks((tasks) => [
+        ...tasks,
+        {
+          id: crypto.randomUUID(),
+          title: text,
+          isDone: false,
+        },
+      ]);
+      setError("");
+      setText("");
+    }
   };
 
   return (
-    <div className="InputTask">
-      <input
-        value={text}
-        onChange={handleChange}
-        placeholder="Create task"
-      ></input>
-      <button onClick={handleClick}>Add Task</button>
-    </div>
+    <>
+      {" "}
+      <div className="InputTask">
+        <input
+          value={text}
+          onChange={handleChange}
+          placeholder="Create task"
+          style={{
+            borderColor: error ? "red" : undefined,
+          }}
+        ></input>
+        <button onClick={handleClick}>Add Task</button>
+      </div>
+      {error && <div className="error">{error}</div>}
+    </>
   );
 };
 export default InputTask;
